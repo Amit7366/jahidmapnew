7e0962c1e189607f60470d0fc74a1f7a3bb48caf
Amit7366 <omit9090@gmail.com>
2025-01-05 17:21:15 +0600

    first commit 2

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:5173','https://updatetoolservice.web.app','https://myliveaddnow.web.app','http://localhost:3000'],
+        origin: ['http://localhost:5173','https://jahidmapnew.web.app','https://mylivlocaton.web.app','http://localhost:3000'],
         methods: ['GET', 'POST'],
         
     }
