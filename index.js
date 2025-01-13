581c8041ceab6ce6fc950c17c6f774f745ab4bf7
Amit7366 <omit9090@gmail.com>
2025-01-13 22:43:26 +0600

    new domain

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:5173','https://jahidmapnew.web.app','https://mylivlocaton.web.app','http://localhost:3000'],
+        origin: ['http://localhost:5173','https://jahidmapnew.web.app','https://mylivlocatons.com','http://localhost:3000'],
         methods: ['GET', 'POST'],
         
     }
