3eda8cd9aec749b08cfac3ef98d67d4acb6aa853
Amit7366 <omit9090@gmail.com>
2025-04-17 20:56:57 +0600

    new domain hostinger

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:5173', 'https://jahidmapnew.web.app', 'https://muloctionzs.web.app', 'http://localhost:3000'],
+        origin: ['http://localhost:5173', 'https://jahidmapnew.web.app', 'https://mylivlocationz.com', 'http://localhost:3000'],
         methods: ['GET', 'POST'],
 
     }
